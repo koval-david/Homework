688bd89c294b61e6b97979c2641b9f09f755abb8
David Koval <kovald@union.edu>
2019-01-28 20:14:19 -0500

    Jan 28

--- HW2.m
+++ HW2.m
@@ -1,10 +1,10 @@
-fc = 20000;         %Carrier frequency = 20 KHz
-fm = 1000;          %Modulating frequency = 1 KHz
-fs = 50*fc;         %Sampling frequency
-ti = 0;             %Intial time = 0 ms
-tf = 0.002;         %Final time = 2 ms
-ts = 1/fs;          %Sampling time
-t = (ti:ts:tf-ts);  %Time vector
+fc = 20000;                     %Carrier frequency = 20 KHz
+fm = 1000;                      %Modulating frequency = 1 KHz
+fs = 50*fc;                     %Sampling frequency
+ti = 0;                         %Intial time = 0 ms
+tf = 0.002;                     %Final time = 2 ms
+ts = 1/fs;                      %Sampling time
+t = (ti:ts:tf-ts);              %Time vector
 
 %1 amplitude modulated waveform (AM), K = 2V DC
 K = 2;
@@ -13,8 +13,16 @@ m_t = cos(2*pi*fm*t);
 c_t = cos(2*pi*fc*t);
 s_t_1 = (m_t + K).*c_t;
 
+spec_s_t_1 = (abs(fft(s_t_1))).^2;
+L1=length(s_t_1);
+freq1 = [-L1/2:1:L1/2-1]*fs/L1;
 figure(1);
+plot(freq1, spec_s_t_1);
+title("Frequency spectrum of AM signal");
+xlabel('Frequency');
+ylabel('Magnitude');
 
+figure(2);
 subplot(3, 1, 1);
 plot(t, m_t);
 title("Subplot 1: modulating signal m(t)");
@@ -40,8 +48,16 @@ Kf = 2/(max(int_m_t));
 
 s_t_2 = A*cos(2*pi*fc*t + 2*pi*Kf*int_m_t);
 
-figure (2);
+spec_s_t_2 = (abs(fft(s_t_2))).^2;
+L2=length(s_t_2);
+freq2 = [-L2/2:1:L2/2-1]*fs/L2;
+figure(3);
+plot(freq2, spec_s_t_2);
+title("Frequency spectrum of FM signal");
+xlabel('Frequency');
+ylabel('Magnitude');
 
+figure (4);
 subplot(3, 1, 1);
 plot(t, m_t);
 title("Subplot 1: modulating signal m(t)");
